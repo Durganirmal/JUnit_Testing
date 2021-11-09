1463f50eefe4ad4456f4bfb0c790dec627aa9b56
Durga Nirmal <durganirmal123@gmail.com>
2021-11-09 19:44:30 +0530

    Added :calculate squareroot

--- SquareRoot.java
+++ SquareRoot.java
@@ -1,9 +1,28 @@
 package com.JUnit_Testing;
 
-
+import java.util.Scanner;
 public class SquareRoot {
 
-	public static void main(String[] args) {
-		 System.out.print("Welcome to JUnit Testing Program !!");
-	}
+	public static Scanner sc =new Scanner(System.in);
+    public static void main(String[] args) {
+        sqrt();
+    }
+    public static void sqrt()
+    {
+        System.out.println("\n Enter the non negative number:");
+        int c=sc.nextInt();
+
+        if(c>0)
+        {
+            double epsilon = Math.pow(10, (-15));
+            double t = c;
+            while (Math.abs(t - c / t) > epsilon * t) {
+                t = (c / t + t) / 2.0;
+            }
+            System.out.println("\n The Absolute value is "+t);
+        }
+        else
+            System.out.println("\n you entered negative number!!!");
+    }
+
 }
